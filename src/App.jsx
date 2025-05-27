f954152c38931060e6c3e3137decaf994947112b
mateo <mateo.usuga.v21@gmail.com>
2025-05-26 22:07:35 -0500

    cambios hechos en app.jsx para recibir los datos de la carpeta componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Perfil from "./components/Perfil";
+import CabeceraCV from "./components/CabeceraCV";
+import Educacion from "./components/Educacion";
+import Perfil from "./components/Perfil";
+import Experiencia from "./components/Experiencia";
+
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <CabeceraCV />
+        <Perfil />
+        <Educacion />
+        <Experiencia />
+      </div>
     </>
   );
 }
