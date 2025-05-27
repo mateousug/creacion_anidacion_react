4cdf20f4723b1b8f0c05f07acc1eccd14d72c0c2
mateo <mateo.usuga.v21@gmail.com>
2025-05-26 22:12:05 -0500

    Feat: componente de CabeceraCV con datos actualizados

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,8 +1,15 @@
 export default function CabeceraCV() {
   return (
     <header>
-      <h1>Nombre del Aprendiz</h1>
-      <h2>Desarrollador UI Frontend</h2>
+      <h1>Nombre: Mateo Usuga Vasco</h1>
+      <h2>Desarrollador fullstack junior</h2>
+        <p>
+            <strong>Teléfono:</strong> +57 324 3815639
+        </p>
+        <p>
+            <strong>Correo:</strong>
+            <a href="mateo.usuga.v21@gmail.com"></a>
+        </p>
     </header>
   );
 }
