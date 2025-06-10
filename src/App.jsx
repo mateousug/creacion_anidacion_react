8664fe27481657ad35f5f9f7eae8ee92d0e087aa
mateo <mateo.usuga.v21@gmail.com>
2025-06-09 20:00:49 -0500

    feat extraccion de datos personales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
-import Perfil from "./components/Perfil";
+import React from "react";
+import {
+  cabecera,
+  perfil,
+  educacion,
+  experiencia,
+  stackTecnologias,
+  proyectos,
+  habilidades
+} from "./data/cvData";
 import CabeceraCV from "./components/CabeceraCV";
+import Perfil from "./components/Perfil";
 import Educacion from "./components/Educacion";
 import Experiencia from "./components/Experiencia";
-import StackTecnologias from "./components/StackTecnologias.jsx";
+import StackTecnologias from "./components/StackTecnologias";
+import Proyectos from "./components/proyectos";
+import Habilidades from "./components/Habilidades";
 
 function App() {
   return (
-    <>
-      <div>
-        <CabeceraCV />
-        <Perfil />
-        <Educacion />
-        <Experiencia />
-        <StackTecnologias />
-      </div>
-    </>
-  );
-}
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      {/* Cabecera */}
+      <CabeceraCV
+        nombre={cabecera.nombre}
+        profesion={cabecera.profesion}
+        telefono ={cabecera.telefono}
+        correo={cabecera.correo}
+      />
 
+      {/* Perfil */}
+      <Perfil texto={perfil.texto}
+      habilidadesBlandas={perfil.habilidadesBlandas} />
+
+      {/* Educación */}
+      <Educacion estudios={educacion} />
+
+      {/* Experiencia */}
+      <Experiencia trabajos={experiencia} />
+
+      {/* Stack de Tecnologías */}
+      <StackTecnologias tecnologias={stackTecnologias} />
+
+      {/* Proyectos (nuevo componente) */}
+      <Proyectos proyectos={proyectos} />
+
+      {/* Habilidades (nuevo componente) */}
+      <Habilidades habilidades={habilidades} />
+    </div>
+  );
+};
 export default App
